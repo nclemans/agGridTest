dd780136fb45b3859322a551c6319c3382c6b574
Nathan Clemans (nclemans) <nclemans@micron.com>
2020-09-15 09:25:58 -0600

    agGridTest 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agGrid-test2';
+  title = 'agGrid-test';
+
+
+  
+  columnDefs = [
+    {headerName: 'Make', field: 'make' },
+    {headerName: 'Model', field: 'model' },
+    {headerName: 'Price', field: 'price'}
+];
+
+rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LicenseManager } from 'ag-grid-enterprise';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+
+LicenseManager.setLicenseKey(
+  'Key'
+);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
